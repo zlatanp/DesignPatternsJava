eb3b3d19eb6298477b7d1564ac69fb8197b3eb52
Zlatan <zlatanprecanica@gmail.com>
2022-01-07 13:48:42 +0000

    Delete unused packages

--- src/adapter/AdvancedMediaPlayer.java
+++
@@ -1,6 +0,0 @@
-package adapter;
-
-public interface AdvancedMediaPlayer {
-    public void playMp4(String fileName);
-    public void playVlc(String fileName);
-}
--- src/factory/Square.java
+++
@@ -1,8 +0,0 @@
-package factory;
-
-public class Square implements Shape {
-    @Override
-    public void draw() {
-        System.out.println("Hi I'm Square");
-    }
-}
